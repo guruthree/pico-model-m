17afd07d00028e1e5f20612f95007f44753cb826
Fred <guru.three@gmail.com>
2021-12-28 18:21:00 +0000

    removed unneeded Arduino stub code

--- includes/Adafruit_USBD_CDC-stub.h
+++ includes/Adafruit_USBD_CDC-stub.h
@@ -1,27 +1,30 @@
 /*
-  Adafruit_USBD_CDC-stub.h - CDC device class stub for Adafruit TinyUSB
-                             Minimum stand in for functionality
-  Copyright (c) 2021 guruthree.  All right reserved.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ * Adafruit_USBD_CDC-stub.h - CDC device class stub for Adafruit TinyUSB
+ *                            Minimum stand in for functionality
+ * Copyright (c) 2021 guruthree
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
 
 #ifndef ADAFRUIT_USBD_CDC_H_
 #define ADAFRUIT_USBD_CDC_H_
-//garbage
-
 
 class Adafruit_USBD_CDC {
     public:
@@ -32,5 +35,3 @@ void Adafruit_USBD_CDC::begin(uint32_t baud){};
 #endif
 
 class Serial{} Serial;
-
-
